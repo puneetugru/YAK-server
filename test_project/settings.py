a3c6db8f4b79845e329fffc6a9dba2d29b0dd25d
puneetugru@gmail.com <puneetugru@gmail.com>
2017-09-11 18:51:43 +0530

    Modified YAK Server

--- test_project/settings.py
+++ test_project/settings.py
@@ -235,7 +235,7 @@ REST_FRAMEWORK = {
     'PAGINATE_BY': 20,
     'PAGE_SIZE': 20,
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'oauth2_provider.ext.rest_framework.OAuth2Authentication',
+		'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': (
